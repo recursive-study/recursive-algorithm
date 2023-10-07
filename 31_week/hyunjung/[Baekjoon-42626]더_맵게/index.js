5fd60db1f05894482ec2afdb2ab97a5df249b5f3
sena-22 <senika_@naver.com>
2023-10-07 22:45:21 +0900

    [Baekjoon-42626]더_맵게

--- 31_week/hyunjung/[Baekjoon-42626]더_맵게/index.js
+++ 31_week/hyunjung/[Baekjoon-42626]더_맵게/index.js
@@ -15,6 +15,10 @@ class MinHeap {
     this.heap.push(value)
     let curIdx = this.heap.length - 1
 
+    /**
+     * 1. 요소가 두 개 이상이고 부모(Math.floor((curIdx - 1) / 2)보다 자식(curIdx)의 값이 더 작으면 바꿔준다(오름차순 정렬)
+     * 2. 검사할 인덱스(curIdx)를 부모의 인덱스로 바꿔준다
+     */
     while (curIdx > 0 && this.heap[curIdx] < this.heap[Math.floor((curIdx - 1) / 2)]) {
       this.swap(curIdx, Math.floor((curIdx - 1) / 2))
       curIdx = Math.floor((curIdx - 1) / 2)
@@ -29,12 +33,17 @@ class MinHeap {
     this.heap[0] = this.heap.pop()
     let curIdx = 0
 
+    /**
+     * 왼쪽 자식(curIdx*2+1)과 오른쪽 자식(curIdx*2+2)을 비교해서 더 작은 자식을 부모와 교체
+     */
     while (curIdx * 2 + 1 < this.heap.length) {
+      // 왼쪽 자식이 존재하면
       let minChildIdx =
         curIdx * 2 + 2 < this.heap.length && this.heap[curIdx * 2 + 2] < this.heap[curIdx * 2 + 1]
           ? curIdx * 2 + 2
           : curIdx * 2 + 1
 
+      // 가장 작은 자식보다 부모자식이 작으면 멈추기
       if (this.heap[curIdx] < this.heap[minChildIdx]) {
         break
       }
@@ -56,6 +65,8 @@ function solution(scoville, K) {
     minHeap.push(s)
   }
 
+  // console.log(minHeap) // MinHeap { heap: [ 1, 2, 3, 9, 10, 12 ] }
+
   let count = 0
 
   while (minHeap.size() >= 2 && minHeap.peek() < K) {
@@ -67,5 +78,7 @@ function solution(scoville, K) {
     count++
   }
 
-  return minHeap.peek() >= K ? count : -1
+  return minHeap.peek() >= K ? count : -1 // 가장 앞에 있는 요소(최솟값)가 스코빌 지수(K)를 넘으면 횟수 반환
 }
+
+console.log(solution([1, 2, 3, 9, 10, 12], 7))
