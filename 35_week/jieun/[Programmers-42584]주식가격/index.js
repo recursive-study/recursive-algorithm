c58dbef731435b37702b6db17fdc064ac1b2d452
JaneChun <janechun22@gmail.com>
2023-12-06 22:34:02 +0900

    [Programmers-42584]주식가격

---
+++ 35_week/jieun/[Programmers-42584]주식가격/index.js
@@ -0,0 +1,12 @@
+function solution(prices) {
+	const result = [];
+	for (let i = 0; i < prices.length; i++) {
+		let stack = 0;
+		for (let j = i + 1; j < prices.length; j++) {
+			stack++;
+			if (prices[i] > prices[j]) break;
+		}
+		result.push(stack);
+	}
+	return result;
+}
